564921c34ba9315fcb0923badc91620ef890692d
petrnahlovsky <petr.nahlovsky@coreit.cz>
2026-09-22 16:31:15 +0200

    Update __init__.py

--- custom_components/nicegate/__init__.py
+++ custom_components/nicegate/__init__.py
@@ -21,6 +21,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         entry.data["mac"],
         entry.data["username"],
         entry.data["password"],
+        entry.data.get("source"),
     )
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
