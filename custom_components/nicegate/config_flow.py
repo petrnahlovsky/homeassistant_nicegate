c6a7afbb75bda3749a7cceda8e39a95adf363c15
petrnahlovsky <petr.nahlovsky@coreit.cz>
2026-09-26 08:24:47 +0200

    Update config_flow.py

--- custom_components/nicegate/config_flow.py
+++ custom_components/nicegate/config_flow.py
@@ -20,7 +20,7 @@ from homeassistant.helpers.selector import (
     TextSelectorType,
 )
 
-from .const import DOMAIN
+from .const import CONF_RELOAD_INTERVAL, DEFAULT_RELOAD_INTERVAL, DOMAIN
 from .nice_api import NiceGateApi
 from .mynice_cloud import (
     MyNiceCloudAuthError,
@@ -54,6 +54,11 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     VERSION = 1
     data: dict[str, Any]={}
+
+    @staticmethod
+    def async_get_options_flow(config_entry):
+        """Return the options flow."""
+        return NiceOptionsFlow()
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
@@ -198,6 +203,29 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             raise CannotConnect
 
 
+
+
+class NiceOptionsFlow(config_entries.OptionsFlow):
+    """Options for Nice gate."""
+
+    async def async_step_init(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Manage the options."""
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        current = self.config_entry.options.get(
+            CONF_RELOAD_INTERVAL, DEFAULT_RELOAD_INTERVAL
+        )
+        schema = vol.Schema(
+            {
+                vol.Required(CONF_RELOAD_INTERVAL, default=current): vol.All(
+                    vol.Coerce(int), vol.Range(min=0, max=1440)
+                ),
+            }
+        )
+        return self.async_show_form(step_id="init", data_schema=schema)
 
 
 class CannotConnect(HomeAssistantError):
