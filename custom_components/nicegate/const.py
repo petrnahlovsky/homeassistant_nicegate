8f068fb51ef5d3b207e713f6a8f1b57b205ff7f6
petrnahlovsky <petr.nahlovsky@coreit.cz>
2026-09-26 08:25:10 +0200

    Update const.py

--- custom_components/nicegate/const.py
+++ custom_components/nicegate/const.py
@@ -1,3 +1,7 @@
 """Constants for the Nice gate integration."""
 
 DOMAIN = "nicegate"
+
+# Periodic reload of the whole integration, in minutes. 0 disables it.
+CONF_RELOAD_INTERVAL = "reload_interval"
+DEFAULT_RELOAD_INTERVAL = 10
